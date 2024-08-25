d86693484762a7bf11b77338d9298dfae369780b
Colin Kierans <vindexus@gmail.com>
2024-08-24 23:13:27 -0700

    Updates

---
+++ _app/immutable/entry/start.DU-lCwoX.js
@@ -0,0 +1,1 @@
+import{d as a}from"../chunks/entry.CP_q9p9f.js";export{a as start};
--- src/lib/mods.ts
+++
@@ -1,5 +0,0 @@
-import {type ModKey, SRC_MODS, SRC_MODS_LIST} from "$lib/mods-consts";
-import type {Mod} from "$lib/mega";
-
-export const MODS = SRC_MODS as Record<ModKey, Mod>
-export const MODS_LIST = SRC_MODS_LIST as Mod[]
